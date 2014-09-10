b96770b6e5412b7f92411dfe87d0ad123c8f3d0c
mikehand <mikehand@umich.edu>
2014-09-10 15:54:35 -0400

    Update reset_upcoming_events.py

--- event_cal/management/commands/reset_upcoming_events.py
+++ event_cal/management/commands/reset_upcoming_events.py
@@ -1,10 +1,12 @@
 from django.core.management.base import BaseCommand
-from django.template import RequestContext, loader
+from django.template import  loader
+from django.utils import timezone
 
 from event_cal.models import CalendarEvent
 
 class Command(BaseCommand):
     def handle(self,*args,**options):
+        now = timezone.localtime(timezone.now())
         upcoming_events=CalendarEvent.get_upcoming_events(True)
-        upcoming_html=loader.get_template('event_cal/upcoming_events.html').render(RequestContext(request,{'upcoming_events':upcoming_events,}))
+        upcoming_html=loader.render_to_string('event_cal/upcoming_events.html',{'upcoming_events':upcoming_events,'now',now})
         cache.set('upcoming_events_html',upcoming_html)
