de9cf296016dfd6b845001c27c2ebd2812f25bf5
mikehand <mikehand@umich.edu>
2014-09-10 15:55:08 -0400

    Update reset_upcoming_events.py

--- event_cal/management/commands/reset_upcoming_events.py
+++ event_cal/management/commands/reset_upcoming_events.py
@@ -8,5 +8,5 @@ class Command(BaseCommand):
     def handle(self,*args,**options):
         now = timezone.localtime(timezone.now())
         upcoming_events=CalendarEvent.get_upcoming_events(True)
-        upcoming_html=loader.render_to_string('event_cal/upcoming_events.html',{'upcoming_events':upcoming_events,'now',now})
+        upcoming_html=loader.render_to_string('event_cal/upcoming_events.html',{'upcoming_events':upcoming_events,'now':now})
         cache.set('upcoming_events_html',upcoming_html)
