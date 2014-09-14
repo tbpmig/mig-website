909e61a2f1340d8ff89b0abf75e27ac5b34e7a1e
mikehand <mikehand@umich.edu>
2014-09-14 01:09:03 -0400

    Clear update scripts

--- updates.py
+++ updates.py
@@ -1,12 +1,2 @@
 print "processing python updates..."
-from event_cal.models import CalendarEvent,EventShift
-from mig_main.models import AcademicTerm
-
-# Put the interviews on campus
-term=AcademicTerm.get_current_term()
-interview_shifts =EventShift.objects.filter(event__term=term,event__event_type__name__contains='Interview')
-for shift in interview_shifts:
-    shift.on_campus=True
-    shift.save()
-
 print "finished processing python updates."
