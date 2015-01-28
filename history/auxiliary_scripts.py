08f59bf14ebb69b4995ea5c28f116ad767e61625
mikehand <mikehand@umich.edu>
2015-01-27 21:23:34 -0500

    Working on #19

--- history/auxiliary_scripts.py
+++ history/auxiliary_scripts.py
@@ -7,11 +7,28 @@ from datetime import datetime,date,timedelta
 from decimal import Decimal
 
 
-from history.models import NonEventProject,NonEventProjectParticipant,ProjectReport,NonEventParticipantAlt
-from requirements.models import ProgressItem
+from history.models import NonEventProject,NonEventProjectParticipant,ProjectReport,NonEventParticipantAlt,Distinction
+from requirements.models import ProgressItem,DistinctionType
 from mig_main.models import AcademicTerm,OfficerPosition,Status,UserProfile,MemberProfile
+from mig_main.utility import get_previous_full_term
 
 cal_dict = {v:k for k,v in enumerate(calendar.month_name)}
+def add_da_pa_status_manually(uniqname,distinction,terms):
+    user = MemberProfile.objects.get(uniqname=uniqname)
+    term = AcademicTerm.objects.get(year=2014,semester_type__name='Fall')
+    while terms>user.get_num_terms_distinction(distinction):
+        if not Distinction.objects.filter(member=user,term=term,distinction_type=distinction).exists():
+            d = Distinction(member=user,term=term,distinction_type=distinction)
+            if distinction.name=='Active':
+                d.gift='N/A'
+            else:
+                d.gift='Unknown'
+            d.save()
+        term=get_previous_full_term(term)
+        if term < user.init_term:
+            print 'ran out of terms'
+            break
+        
 def add_tutoring_to_project_report(id_num):
     pr = NonEventProject.objects.get(id=id_num)
     term = AcademicTerm.objects.get(semester_type__name='Winter',year=2014)
