363f80fb9fbe8e468f7d4c93e0cbcfdc0819701a
mikehand <mikehand@umich.edu>
2015-03-14 17:16:13 +0000

    wsgi changes for new virtualenv magic -kyle

--- migweb/wsgi.py
+++ migweb/wsgi.py
@@ -13,13 +13,25 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
+
+# virtualenv magic #yolo
+activate_env='/home/webdev/.virtualenvs/migweb/bin/activate_this.py'
+execfile(activate_env, dict(__file__=activate_env))
+
 import os
+
+import sys
+path = '/srv/www'
+if path not in sys.path:
+    sys.path.append(path)
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "migweb.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "migweb.settings")
+
+
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
@@ -30,3 +42,9 @@ application = get_wsgi_application()
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
+
+# From https://devcenter.heroku.com/articles/django-memcache
+# Fix django closing connection to MemCachier after every request (#11331)
+from django.core.cache.backends.memcached import BaseMemcachedCache
+BaseMemcachedCache.close = lambda self, **kwargs: None
+
