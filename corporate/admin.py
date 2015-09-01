b4040ec2c53e5b332084141d774e88c16ccfd8b7
mikehand <mikehand@umich.edu>
2015-08-31 23:05:10 -0400

    Update Corporate Admin

--- corporate/admin.py
+++ corporate/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from corporate.models import CorporateTextField,CorporateResourceGuide
+from corporate.models import CorporateTextField,CorporateResourceGuide,Company,JobField
 
 admin.site.register(CorporateTextField)
 admin.site.register(CorporateResourceGuide)
+admin.site.register(Company)
+admin.site.register(JobField)
