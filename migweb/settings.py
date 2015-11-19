813d46b19371a2440ac6c0c24b4374ceaebc53ac
mikehand <mikehand@umich.edu>
2015-11-19 18:27:41 -0500

    disable ERD generation

--- migweb/settings.py
+++ migweb/settings.py
@@ -117,7 +117,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_spaghett',
+    #'django_spaghetti',
     # Uncomment the next line to enable the admin:
      'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
