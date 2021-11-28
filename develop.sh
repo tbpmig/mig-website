18ccb1a65b4726c2bb80eff50f72aa615aad5f18
Link Li <link@langchao.org>
2021-11-28 03:35:07 -0800

    Initialize local settings when it doesn’t exist

--- develop.sh
+++ develop.sh
@@ -4,6 +4,11 @@ REGION=us-east-2
 
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REGISTRY
 
+if [ ! -f "migweb/local_settings.py" ]; then
+  echo "migweb/local_settings.py does not exist. Creating one from the template."
+  cp migweb/local_settings.template migweb/local_settings.py
+fi
+
 if [ -z "$1" ]
   then
     docker-compose up
